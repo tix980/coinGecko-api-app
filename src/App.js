829e9ffc654659ab517cb2578ae46d789ff8459e
tingwei xie <tix980@mail.usask.ca>
2022-02-07 16:38:13 -0500

    fetch the crpyto api and rendered in the list form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//DECLARE DATA STATE VARIABLE
+	const [data, setData] = useState([]);
+
+	// DEFINE THE TITLE OF THE LIST
+	const title = "Top 10 Crypto Exchange Platform";
+
+	//RENDER EACH EXCHANGE INFO OBJECT IN THE TABLE BODY
+	const exchangeList = data.map(platform => (
+		<tr key={platform.name}>
+			<td>{platform.trust_score_rank}</td>
+			<td><img src={platform.image} alt={platform.name} /><span>{platform.name}</span></td>
+			<td>{platform.trust_score}</td>
+			<td>{platform.country}</td>
+			<td>{platform.url}</td>
+		</tr>
+	))
+
+	//FETCH THE COINGECKO API AND ACCESS THE TOP 10 EXCHANGE INFORMATION
+	useEffect(() => {
+		{
+			var requestOptions = {
+				method: 'GET',
+				redirect: 'follow'
+			};
+
+			fetch("https://api.coingecko.com/api/v3/exchanges?per_page=10", requestOptions)
+				.then(response => response.json())
+				.then(result => setData(result))
+				.catch(error => console.log('error', error));
+		}
+		console.log(data);
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>{title}</h1>
+				<table>
+					<tr>
+						<th>Rank</th>
+						<th>Exchange</th>
+						<th>Trust Score</th>
+						<th>country</th>
+						<th>Website Link</th>
+					</tr>
+					{exchangeList}
+				</table>
+			</header>
+
+		</div>
+	);
 }
 
 export default App;
