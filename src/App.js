7c293bb71325afbe1043b04ab3754ab864698706
tingwei xie <tix980@mail.usask.ca>
2022-02-12 18:44:51 -0500

    create another page that can be accessed by clicking the selected platform

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import {Link, Route, Routes, useParams} from "react-router-dom";
+import Platform from "./Platform";
 
-
-
-function App() {
+//A FUNCTION THAT DISPLAY THE TOP 10 EXCHANGE LIST
+function ExchangeList() {
 	//DECLARE DATA STATE VARIABLE
 	const [data, setData] = useState([]);
-
 	// DEFINE THE TITLE OF THE LIST
 	const title = "Top 10 Crypto Exchange Platform";
 
+
 	//RENDER EACH EXCHANGE INFO OBJECT IN THE TABLE BODY
 	const exchangeList = data.map(platform => (
-		<tr key={platform.name}>
-			<td>{platform.trust_score_rank}</td>
-			<td><img src={platform.image} alt={platform.name} /><span>{platform.name}</span></td>
-			<td>{platform.trust_score}</td>
-			<td>{platform.country}</td>
-			<td>{platform.url}</td>
-		</tr>
-	))
+			<tr key={platform.id}>
+				<td>{platform.trust_score_rank}</td>
+
+				<Link to={`/${platform.id}`}>
+					<td><img src={platform.image} alt={platform.name}/>
+					<span>{platform.name}</span></td>
+				</Link>
+				<td>{platform.trust_score}</td>
+				<td>{platform.country}</td>
+				<td className="url"><a href={platform.url}>{platform.url}</a></td>
+			</tr>
+		)
+	)
 
 	//FETCH THE COINGECKO API AND ACCESS THE TOP 10 EXCHANGE INFORMATION
 	useEffect(() => {
 		{
-			var requestOptions = {
+			let requestOptions = {
 				method: 'GET',
 				redirect: 'follow'
 			};
@@ -34,14 +40,14 @@ function App() {
 				.then(result => setData(result))
 				.catch(error => console.log('error', error));
 		}
-		console.log(data);
-	}, []);
+	}, [data]);
 
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h1>{title}</h1>
 				<table>
+					<thead>
 					<tr>
 						<th>Rank</th>
 						<th>Exchange</th>
@@ -49,11 +55,27 @@ function App() {
 						<th>country</th>
 						<th>Website Link</th>
 					</tr>
+					</thead>
+					<tbody>
 					{exchangeList}
+					</tbody>
+
 				</table>
 			</header>
-
 		</div>
+	)
+}
+
+
+
+
+function App() {
+
+	return (
+		<Routes>
+			<Route path="/" element={<ExchangeList/>}/>
+			<Route path="/:id" element={<Platform/>}/>
+		</Routes>
 	);
 }
 
---
+++ src/Platform.js
@@ -0,0 +1,54 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
+
+
+
+function Platform() {
+	//ACCESS THE SELECTED PLATFORM'S PARAMETERS
+	let params = useParams();
+
+	//DECLARE DETAIL DATA STATE VARIABLE
+	const [detailData, setDetailData] = useState([]);
+
+
+	//FETCH THE COINGECKO API AND ACCESS THE SELECTED EXCHANGE PLATFORM INFORMATION
+	// WITH SELECTED PLATFORM ID
+	useEffect(() => {
+		{
+			let requestOptions = {
+				method: 'GET',
+				redirect: 'follow'
+			};
+
+			fetch(`https://api.coingecko.com/api/v3/exchanges/${params.id}`, requestOptions)
+				.then(response => response.json())
+				.then(result => setDetailData(result))
+				.catch(error => console.log('error', error));
+		}
+	}, [detailData]);
+
+	//DISPLAY THE SELECTED PLATFORM INFORMATION
+	const selectedExchangeDetail =
+		<div>
+			<h1>{detailData.id} Detail Page</h1>
+			<div>{detailData.name}</div>
+			<div>{detailData.country}</div>
+			<div><img src={detailData.image} /></div>
+			<div>{detailData.trust_score_rank}</div>
+			<div>{detailData.year_established}</div>
+			<div>{detailData.facebook_url}</div>
+			<div>{detailData.reddit_url}</div>
+			<div>{detailData.description}</div>
+		</div>;
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{selectedExchangeDetail}
+			</header>
+		</div>
+	);
+}
+
+export default Platform;
